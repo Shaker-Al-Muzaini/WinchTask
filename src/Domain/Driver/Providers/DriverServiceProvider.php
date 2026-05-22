9d8020dae8f218140874a853f636843eef804040
Shaker-Al-Muzaini <sshakiralmazini@gmail.com>
2026-05-22 17:21:37 +0300

    v1

---
+++ src/Domain/Driver/Providers/DriverServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Src\Domain\Driver\Providers; // Driver وليس Drivers
+
+use Illuminate\Support\ServiceProvider;
+
+class DriverServiceProvider extends ServiceProvider
+{
+    public function boot(): void {}
+}
